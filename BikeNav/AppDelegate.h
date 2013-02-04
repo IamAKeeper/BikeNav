fce90ca202a6897c6688e45f53dcfd0bd6968a4e
Russell Wyatt <rswyatt1@gmail.com>
2013-02-04 12:41:40 -0800

    Added RestKit to app

--- BikeNav/AppDelegate.h
+++ BikeNav/AppDelegate.h
@@ -7,9 +7,21 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Availability.h>
+#if __IPHONE_OS_VERSION_MIN_REQUIRED
+#import <SystemConfiguration/SystemConfiguration.h>
+#import <MobileCoreServices/MobileCoreServices.h>
+#else
+#import <SystemConfiguration/SystemConfiguration.h>
+#import <CoreServices/CoreServices.h>
+#endif
+
+// If using Core Data, you may also wish to append
+#import <CoreData/CoreData.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+
 
 @end
